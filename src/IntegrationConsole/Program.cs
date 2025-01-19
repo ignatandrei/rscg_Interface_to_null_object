39491eaca5fbab9d532b7381396ef5e4a79e0170
Andrei Ignat <ignatandrei@yahoo.com>
2025-01-19 11:07:13 +0200

    first version

--- src/IntegrationConsole/Program.cs
+++ src/IntegrationConsole/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using IntegrationConsole;
+
 Console.WriteLine("Hello, World!");
+IDepartment department= new Department_null();
+department.Name = "IT";
+IEmployee employee = new Employee_null();
+employee.FirstName = "Andrei";
+employee.Department=department;
+Console.WriteLine(employee.FirstName);
+Console.WriteLine(employee.Department.Name);
--- src/rscg_Interface_to_null_object/DataFromExposeInterface.cs
+++ src/rscg_Interface_to_null_object/DataFromExposeInterface.cs
@@ -6,14 +6,22 @@ internal class DataFromExposeInterface
 {
     public string Name { get; private set; }
 
-    private INamedTypeSymbol type;
+    internal IPropertySymbol[] props;
+    public IMethodSymbol[] functions;
+    //private INamedTypeSymbol type;
 
     public string FullName { get; private set; }
     public DataFromExposeInterface(INamedTypeSymbol type)
     {
-        this.type = type;
+        //this.type = type;
         FullName = type.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat);
         Name = type.Name;
-
+        props = type.GetMembers()
+                .OfType<IPropertySymbol>()
+                .ToArray();
+        functions = type.GetMembers()
+                .OfType<IMethodSymbol>()
+                .Where(x => x.MethodKind == MethodKind.Ordinary)
+                .ToArray();
     }
 }
